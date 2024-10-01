65c208c2fd62f881d91c6c81e24f46176ccb1ed7
zenin5479 <zenin5479@gmail.com>
2024-10-01 21:49:05 +0300

    Update 0.4 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -37,10 +37,10 @@ namespace DivineProportionConsole
 
       public static int GetPercent(int b, int a)
       {
-         if (b != 0)
-            return (int)(a / (b / 100M));
+         if (b == 0) return 0;
+         int r = (int)(a / (b / 100M));
+         return r;
 
-         return 0;
       }
 
    }
